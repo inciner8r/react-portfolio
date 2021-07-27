2508753b295dd58c513b8bcc174bf6d05cced44f
incinerator17 <punarvpawade17@gmail.com>
2021-07-27 16:13:02 +0530

    add links, change bg and moar styling

--- src/components/Navspace/Navspace.jsx
+++ src/components/Navspace/Navspace.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './navspace.scss'
+import { FaGithub, FaLinkedin, FaTwitter, FaTelegram, FaDiscord } from "react-icons/fa";
 
 const Navspace = () => {
     return (
         <div className="navspace">
-            Toast
+            <ul className="links">
+                <a href="https://github.com/inciner8r" target="_blank" rel="noreferrer" className="git"><FaGithub size="32px"/></a>
+                <a href="https://linkedin.com/in/punarvpawade" target="_blank" rel="noreferrer" className="linkedin"><FaLinkedin size="32px"/></a>
+                <a href="https://twitter.com/incinerator_17" target="_blank" rel="noreferrer" className="twitter"><FaTwitter size="32px"/></a>
+                <a href="https://t.me/inciner8r" target="_blank" rel="noreferrer" className="telegram"><FaTelegram size="32px"/></a>
+                <a href="https://discord.com/users/569047479140483082" target="_blank" rel="noreferrer" className="discord"><FaDiscord size="32px"/></a>
+            </ul>
+
         </div>
     )
 }
