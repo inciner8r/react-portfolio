459e92b95594de76d8a6c3aa4fea88896b35b7f2
incinerator17 <punarvpawade17@gmail.com>
2021-07-29 00:31:36 +0530

    fix dark logo due to semi-transparent cover by parent

--- src/components/Navspace/Navspace.jsx
+++ src/components/Navspace/Navspace.jsx
@@ -5,39 +5,49 @@ import { FaGithub, FaLinkedin, FaTwitter, FaTelegram, FaDiscord } from "react-ic
 const Navspace = () => {
     const showLink = (e) => {
         var targ = e.target
-        if(targ.childNodes[1].classList[1] !== "link-active"){
+        if(targ.childNodes[0].childNodes[1].classList[1] !== "link-active"){
                 e.preventDefault()
                     if(document.querySelector(".link-active") !== null){
                         document.querySelector(".link-active").classList.toggle("link-active")
-                        document.querySelector(".link-btn-active").classList.toggle("link-btn-active")
+                        document.querySelector(".link-btn-bg-active").classList.toggle("link-btn-bg-active")
                     }     
-                targ.classList.toggle("link-btn-active")
-                targ.childNodes[1].classList.toggle("link-active")             
+                targ.childNodes[0].classList.toggle("link-btn-bg-active")
+                targ.childNodes[0].childNodes[1].classList.toggle("link-active")             
             }
         }
     
     return (
         <div className="navspace">
             <ul className="links">
-                <a href="https://github.com/inciner8r" target="_blank" rel="noreferrer" className="git link-btn" onClick={showLink}>                     
-                    <FaGithub className="link-icon" size="32px"/>
-                    <div className="link-text">Github</div>
+                <a href="https://github.com/inciner8r" target="_blank" rel="noreferrer" className="git link-btn" onClick={showLink}>
+                    <div className="nav-links-bg">                     
+                        <FaGithub className="link-icon" size="32px"/>
+                        <div className="link-text">Github</div>
+                    </div>
                 </a>
                 <a href="https://linkedin.com/in/punarvpawade" target="_blank" rel="noreferrer" className="linkedin link-btn" onClick={showLink}>
-                    <FaLinkedin className="link-icon" size="32px"/>
-                    <div className="link-text">Linkedin</div>
+                <div className="nav-links-bg">
+                        <FaLinkedin className="link-icon" size="32px"/>
+                        <div className="link-text">Linkedin</div>
+                    </div>
                 </a>
                 <a href="https://twitter.com/incinerator_17" target="_blank" rel="noreferrer" className="twitter link-btn" onClick={showLink}>
-                    <FaTwitter className="link-icon" size="32px"/>
-                    <div className="link-text">Twitter</div>
+                    <div className="nav-links-bg">
+                        <FaTwitter className="link-icon" size="32px"/>
+                        <div className="link-text">Twitter</div>
+                    </div>
                 </a>
                 <a href="https://t.me/inciner8r" target="_blank" rel="noreferrer" className="telegram link-btn" onClick={showLink}>
-                    < FaTelegram className="link-icon" size="32px"/>
-                    <div className="link-text">Telegram</div>
+                    <div className="nav-links-bg">
+                        < FaTelegram className="link-icon" size="32px"/>
+                        <div className="link-text">Telegram</div>
+                    </div>    
                 </a>
                 <a href="https://discord.com/users/569047479140483082" target="_blank" rel="noreferrer" className="discord link-btn" onClick={showLink}>
-                    <FaDiscord className="link-icon" size="32px"/>
-                    <div className="link-text">Discord</div>
+                    <div className="nav-links-bg">
+                        <FaDiscord className="link-icon" size="32px"/>
+                        <div className="link-text">Discord</div>
+                    </div>
                 </a>
             </ul>
         </div>
